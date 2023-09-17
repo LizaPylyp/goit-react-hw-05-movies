ca7daf659920aa42b86b832b05a1211c4124fac1
Liza <lizkapylypchuk123456@gmail.com>
2023-09-18 00:18:23 +0300

    Update MovieList.styled.jsx

--- src/components/MoviesList/MovieList.styled.jsx
+++ src/components/MoviesList/MovieList.styled.jsx
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 const List = styled.ul`
     display: flex;
     flex-wrap: wrap;
-    gap: 5px;
+    gap: 8px;
     list-style: none;
     padding: 0;
 `;
 
 const MovieLink = styled(Link)`
-    font-size: 20px;
+    font-size: 18px;
     text-decoration: none;
 `;
 
 const Button = styled.button`
     width: 150px;
     height: 50px;
-    background-color: blue;
+    background-color: red;
     color: white;
-    border-radius: 4px;
-    margin-bottom: 20px;
+    border-radius: 3px;
+    margin-bottom: 18px;
 `;
 
 export { List, MovieLink, Button };
